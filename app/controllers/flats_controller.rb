a377cd334fdb642bb91e71a09ac573067cbb1e76
Jessie <jessie.baxter29@gmail.com>
2018-11-07 19:38:57 +1100

    Mapped out layout of flats page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,29 @@
 class FlatsController < ApplicationController
+
+	def index
+		@flats = Flat.all
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def destroy
+	end
+
+	def update
+	end
+
+	private
+
+
+
 end
